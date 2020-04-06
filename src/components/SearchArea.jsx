c405fc6982ee8f076d6fd2e0d138b50bf8dc516c
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-06 18:34:54 +1000

    add search area

---
+++ src/components/SearchArea.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchArea(){
+    return (
+        <div>
+        <input className="search-area" name="search" type="text" placeholder="Search" />
+        
+
+    </div>
+    )
+}
+
+export default SearchArea;
