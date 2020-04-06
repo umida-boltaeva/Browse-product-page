bba46b6c55c536135bbea5acd20c70e5a1180ef1
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-06 18:03:52 +1000

    Initial commit

---
+++ src/components/products.js
@@ -0,0 +1,90 @@
+
+
+const products = [
+    {
+      catalog_number: 1,
+      name: "Broccolli",
+      imgURL: "",
+      category: "vegetable",
+      price: 5.50
+    },
+    {
+        catalog_number: 2,
+        name: "Apple",
+        imgURL: "",
+        category: "fruit",
+        price: 4.90
+    },
+    {
+        catalog_number: 3,
+        name: "Apricot",
+        imgURL: "",
+        category: "fruit",
+        price: 3.50
+    },
+    {
+        catalog_number: 4,
+        name: "Carrot",
+        imgURL: "",
+        category: "vegetable",
+        price: 1.50
+    },
+    {
+        catalog_number: 5,
+        name: "Banana",
+        imgURL: "",
+        category: "fruit",
+        price: 4.00
+    },
+    {
+        catalog_number: 6,
+        name: "Mango",
+        imgURL: "",
+        category: "fruit",
+        price: 3.00
+    },
+    {
+        catalog_number: 7,
+        name: "Avocado",
+        imgURL: "",
+        category: "fruit",
+        price: 2.50
+    },
+    {
+        catalog_number: 8,
+        name: "Coke",
+        imgURL: "",
+        category: "drinks",
+        price: 3.50
+    },
+    {
+        catalog_number: 9,
+        name: "Chips",
+        imgURL: "",
+        category: "snacks",
+        price: 2.50
+    },
+    {
+        catalog_number: 10,
+        name: "Grapes",
+        imgURL: "",
+        category: "fruit",
+        price: 6.90
+    },
+    {
+        catalog_number: 11,
+        name: "Lime",
+        imgURL: "",
+        category: "fruit",
+        price: 3.90
+    },
+    {
+        catalog_number: 12,
+        name: "Milk",
+        imgURL: "",
+        category: "milk",
+        price: 1.75
+    }
+  ];
+
+  export default products;
